ec8334d6e5d3b477248930e9a74f2a468edfe5b6
Sebastian Jordan <jordanse@hu-berlin.de>
2014-06-26 18:37:56 +0200

    Reimplement server-side compilation, works with cached resources now.
    Instead of getting the location of a less file to be rendered we now
    get its content and pipe it to the lessc command.

--- src/collective/lesscss/browser/compiledcss.py
+++ src/collective/lesscss/browser/compiledcss.py
@@ -9,9 +9,9 @@ import logging
 import re
 
 
-def render_cachekey(method, self, lessc_command_line, resource_path, resource_file_name):
-    """Cache by resource_path and resource_file_name"""
-    return (resource_path, resource_file_name)
+def render_cachekey(method, self, resource_id):
+    """Cache by resource id"""
+    return resource_id
 
 
 class compiledCSSView(BrowserView):
@@ -23,50 +23,76 @@ class compiledCSSView(BrowserView):
         self.request = request
         self.logger = logging.getLogger('collective.lesscss')
 
-    def __call__(self):
-        portal_less = getToolByName(self.context, 'portal_less')
+    def portal_less(self):
+        return getToolByName(self.context, 'portal_less')
+                    
+
+    @staticmethod
+    def output_obj_info(obj):
+        represent = lambda x: str(x)
+        objType = represent(type(obj))
+        objSignatur = represent(obj)
+        return "Object %s has type %s" % (objType, objSignatur)
+
+    def getInlineLess(self, item_id):
+        """ Get the less code of a registered resource as a string
+
+        item_id: This is the id of the resource that we want to get
+        the content from.
+
+        return: The actual content of the stored resource is returned
+        in a string"""
+        portal_less = self.portal_less()
+        inline_code = portal_less.getInlineResource(item_id, self.context)
+        return inline_code
+
+    def _get_lessc_cmd(self):
         lessc_command_line = os.path.join(os.environ.get('INSTANCE_HOME'), os.path.pardir, os.path.pardir, 'bin', 'lessc')
         if not os.path.exists(lessc_command_line):
             self.logger.error("A valid lessc executable cannot be found."
                          "We are assumming that it has been provided by buildout"
                          "and placed in the buildout bin directory."
                          "If not, you should provide one (e.g. symbolic link) and place it there.")
+        return lessc_command_line
+
+    def __call__(self):
+        portal_less = self.portal_less()
 
         less_resources = portal_less.getEvaluatedResources(self.context)
-        regex = r'^(\+\+[\w_-]+\+\+[\w_-]+)\/(.*)$'
 
         results = []
 
         for less_resource in less_resources:
+            
             res_id = less_resource.getId()
-            find = re.search(regex, res_id)
+            compiled_css = self.compile_less_resource_id(res_id)
 
-            # Just make sure that is a plone.resource object
-            if find:
-                # Extract its resource directory type and name
-                resource_directory_type, resource_file_name = find.groups()
-
-                # Get its directoryResource object and extract the full path
-                resource_path = getUtility(IResourceDirectory, name=resource_directory_type).directory
-
-                results.append('/*    %s    */\n' % res_id)
-
-                results.append(self.renderLESS(lessc_command_line, resource_path, resource_file_name))
-
-                results.append('\n/*    End  %s    */\n' % res_id)
-            else:
-                self.logger.warning("The resource %s is not a valid plone.resource asset, and cannot be server-side compiled." % res_id)
-
+            results.append('/*    %s    */\n' % res_id)
+            results.append(compiled_css)
+            results.append('\n/*    End  %s    */\n' % res_id)
+            
         self.request.response.setHeader('Content-Type', 'text/css')
         return ''.join(results)
 
     @ram.cache(render_cachekey)
-    def renderLESS(self, lessc_command_line, resource_path, resource_file_name):
-        self.logger.info("The resource %s has been server-side compiled." % resource_file_name)
+    def compile_less_resource_id(self, resource_id):
+        resource_inline = self.getInlineLess(resource_id)
+        result = self.compile_less_code(self._get_lessc_cmd(), resource_inline)
+        self.logger.info("The resource %s has been server-side compiled." % resource_id)
+        return result
+
+    def compile_less_code(self, lessc_command_line, less_code):
+        """Compiles less code via the lessc compiler installed in bin/.
+
+        This procedure returns the compiled css code that results of
+        the compilation of the code as a string.  Errors are
+        discarded and not returned back.
+        """
 
         # Call the LESSC executable
-        process = subprocess.Popen([lessc_command_line, os.path.join(resource_path, resource_file_name)],
-                           stdout=subprocess.PIPE)
-        output, errors = process.communicate()
+        process = subprocess.Popen([lessc_command_line, '-'],
+                           stdout=subprocess.PIPE,
+                           stdin=subprocess.PIPE)
+        output, errors = process.communicate(input=less_code)
         # Return the command output
         return output
